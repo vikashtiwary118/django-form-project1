3092595b4328a95c56c8356172ed6ea83213ce7a
vikashtiwary118 <tiwaryvikash118@gmail.com>
2019-10-04 16:35:29 +0530

    add project foulder

---
+++ formproject1/testapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from . import forms
+# Create your views here.
+
+def studentregisterview(request):
+	form=forms.StudentRegistration()
+	return render(request,'testapp/register.html',{'form':form})
